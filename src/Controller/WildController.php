5e53a4ff181c058964454984d830d8469122047d
zazalit <lisa.24@hotmail.fr>
2020-06-08 03:28:39 +0200

    find by

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
+use App\Entity\Category;
 
 /**
  * @Route("/wild", name="wild_")
@@ -27,8 +29,25 @@ class WildController extends AbstractController
             $name = ucwords(str_replace('-', ' ', $slug));
         }
 
+
         return $this->render('wild/show.html.twig', ['name' => $name]);
     }
 
+    /**
+     * @Route("/category/{categoryName}", name="show_category")
+     * @param string $categoryName
+     * @return Response
+     */
+    public function showByCategory(string $categoryName) {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' =>($categoryName)]);
 
+        $series = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category'=> $category], ['id'=>'DESC'], 3 );
+
+        return $this->render('wild/category.html.twig',
+            ['ListOfSeries' => $series]);
+    }
 }
